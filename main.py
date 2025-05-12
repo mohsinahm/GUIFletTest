5786a648d70ce2ed67283a51634ec62a270dffbc
mohsinahm <35180333+mohsinahm@users.noreply.github.com>
2025-05-12 14:41:20 -0700

    6th commit
    adding mount

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flet import *
-import flet_fastapi
+import flet as ft
+import flet.fastapi as flet_fastapi
 
-async def main(page: Page):
-    await page.add_async(Text("Testing is working bro", size=30, weight="bold"))
 
-app = flet_fastapi.app(main)
+async def root_main(page: ft.Page):
+    await page.add_async(ft.Text("This is root app!"))
+
+
+async def sub_main(page: ft.Page):
+    await page.add_async(ft.Text("This is sub app!"))
+
+
+app = flet_fastapi.FastAPI()
+
+
+app.mount("/sub-app", flet_fastapi.app(sub_main))
+app.mount("/", flet_fastapi.app(root_main))
